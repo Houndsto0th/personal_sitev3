ab2ce5a5110e12d84b2634fe875cb5e39eaacba2
Houndsto0th <bry.kinnear@gmail.com>
2015-02-12 11:58:43 -0700

    playing with buttons

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){               
+$(document).ready(function(){
   $('.materialboxed').materialbox();
 });
 $(document).ready(function(){
   $('.materialboxed').materialbox();
+});
+
+$('.btn-ink').on('click', function () {
+  $(this).toggleClass('play');
+  $(this).toggleClass('x');
+});
+
+
+$('.social').on('click', function () {
+  $(this).toggleClass('octicon-flame');
+  $(this).toggleClass('octicon-ruby')
 });
